7aaabfa349a6be2388470bd374368d2ea8630f23
Maidy Conde <maidyladino04@gmail.com>
2024-08-29 12:58:56 -0500

    sheuler

--- Back-end/src/main/java/com/planificadortareas/planificadortareas/Taks/Taks.java
+++ Back-end/src/main/java/com/planificadortareas/planificadortareas/Taks/Taks.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Taks {
-    @Scheduled(cron = "0  13 * * *")
+    @Scheduled(cron = "0  13 * * * *")
     public void sendNotificationcron() {
         SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss aa");
         long miliseconds = System.currentTimeMillis();
